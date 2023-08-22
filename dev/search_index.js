f83e1795d595cb498374ef6386b45022c05c17c7
Documenter.jl <documenter@juliadocs.github.io>
2023-08-22 17:48:24 +0000

    build based on 0364e38

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#TheDerivativeGraph.jl","page":"TheDerivativeGraph.jl","title":"TheDerivativeGraph.jl","text":"","category":"section"},{"location":"","page":"TheDerivativeGraph.jl","title":"TheDerivativeGraph.jl","text":"Documentation for TheDerivativeGraph.jl","category":"page"}]
+}
